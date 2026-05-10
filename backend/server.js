c44a3c97ee76a25d670cc43edcfb53500e854a15
krishna4596 <krishprajapat99977@gmail.com>
2026-05-10 20:50:48 +0530

    fixed cookie samesite issue

--- backend/server.js
+++ backend/server.js
@@ -97,7 +97,7 @@ const csrfProtection = csurf({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax"
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
   } 
 });
 
